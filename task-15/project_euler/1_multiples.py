611f2ca6eebf4121eeeed22ddb194b8cad0b36fa
Prashant9683 <91518700+Prashant9683@users.noreply.github.com>
2021-11-06 12:19:30 +0530

    Update 1_multiples.py

--- task-15/project_euler/1_multiples.py
+++ task-15/project_euler/1_multiples.py
@@ -1,14 +1,9 @@
 t = int(input())
 
-
-def ar(x):
-    return x*(x+1)
-
-
 for i in range(t):
+    sum = 0
     n = int(input())
-    n -= 1
-    a = int(n/3)
-    b = int(n/5)
-    c = int(n/15)
-    print(int(int(3*ar(a) + 5*ar(b) - 15*ar(c)) >> 1))
+    for l in range(n):
+        if (l%3==0 or l%5==0):
+            sum+=l
+    print(sum)

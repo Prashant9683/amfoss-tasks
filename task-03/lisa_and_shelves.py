9b6a7b37d95a3c455dc30a9b4aef678431be2baa
Prashant9683 <91518700+Prashant9683@users.noreply.github.com>
2021-12-25 10:45:26 +0530

    Update lisa_and_shelves.py

--- task-03/lisa_and_shelves.py
+++ task-03/lisa_and_shelves.py
@@ -1,30 +1,11 @@
-from math import sqrt
-
-mod = 1000000007
-def mult(a, b):
-    return ((a % mod) * (b % mod)) % mod
-def calculate_factors(n):
-    cnt = 0
-    ans = 1
-
-    while (n % 2 == 0):
-        cnt += 1
-        n = n // 2
-
-    if (cnt):
-        ans = mult(ans, (cnt + 1))
-    for i in range(3, int(sqrt(n)), 2):
-        cnt = 0
-        while (n % i == 0):
-            cnt += 1
-            n = n // i
-        if (cnt):
-            ans = mult(ans, (cnt + 1))
-    if (n > 2):
-        ans = mult(ans, 2)
-    return ans % mod
-
-if __name__ == '__main__':
-    n = int(input())
-
-    print(calculate_factors(n))
+n = int(input()) 
+count = 0
+i=1
+while (i*i) <= n: 
+    if n % i == 0: 
+        if i*i ==n:
+            count+=1
+            break
+        count+=2
+    i+=1
+print(count)
